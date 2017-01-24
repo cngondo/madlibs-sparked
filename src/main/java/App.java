ce41fd1b843480e74a5758da8440158677f459c5
cngondo <devngondo@gmail.com>
2017-01-24 20:45:22 +0300

    added story.vtl in the templates

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,6 +9,25 @@ import static spark.Spark.*;
 public class App {
 
     public static void main(String[] args) {
+
+    	String layout = "templates/layout.vtl";
+		// To locate the resources for the project
+		// staticFileLocation("/public");
+		//Port Number
+		port(1234);
+		//form route
+    	get("/", (request, response) -> {
+    		Map<String, Object> model = new HashMap<String, Object> ();
+    		model.put("template", "templates/form.vtl");
+    		return new ModelAndView(model, layout);
+    	}, new VelocityTemplateEngine());
+
+    	//story route
+    	get("/story", (request, response) -> {
+    		Map<String, Object> model = new HashMap<String, Object> ();
+    		model.put("template", "templates/story.vtl");
+    		return new ModelAndView(model, layout);
+    	}, new VelocityTemplateEngine());
     	
     }
 }
