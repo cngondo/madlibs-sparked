0d1e9d3633b7aeab32bdac657e9589b217c3d79f
cngondo <devngondo@gmail.com>
2017-01-24 20:51:13 +0300

    Form variables showing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -25,6 +25,13 @@ public class App {
     	//story route
     	get("/story", (request, response) -> {
     		Map<String, Object> model = new HashMap<String, Object> ();
+    		//get the params from the form that is submitted
+    		 model.put("person1", request.queryParams("person1"));
+		      model.put("person2", request.queryParams("person2"));
+		      model.put("animal", request.queryParams("animal"));
+		      model.put("exclamation", request.queryParams("exclamation"));
+		      model.put("verb", request.queryParams("verb"));
+		      model.put("noun", request.queryParams("noun"));
     		model.put("template", "templates/story.vtl");
     		return new ModelAndView(model, layout);
     	}, new VelocityTemplateEngine());
